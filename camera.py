5a332a94b450b7c1baa11c86085813fd82ca9d3f
creismeas Maël <mael.creismeas@outlook.fr>
2024-02-16 14:38:38 +0100

    update

---
+++ camera.py
@@ -0,0 +1,27 @@
+import cv2
+import threading
+
+
+# bufferless VideoCapture Camera_Video
+class VideoCapture:
+    def __init__(self, name):
+        self.cap = cv2.VideoCapture(name)
+        self.lock = threading.Lock()
+        self.t = threading.Thread(target=self._reader)
+        self.t.daemon = True
+        self.t.start()
+
+    # grab frames as soon as they are available
+    def _reader(self):
+        while True:
+            with self.lock:
+                ret = self.cap.grab()
+            #ret : si plus de flux arrete la boucle
+            if not ret:
+                break
+
+    # retrieve latest frame
+    def read(self):
+        with self.lock:
+            _, frame = self.cap.retrieve()
+        return frame
